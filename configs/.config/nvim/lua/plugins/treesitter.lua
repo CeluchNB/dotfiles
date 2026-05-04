e2f21b0e4d856063efe013b7479c927c532f3773
Noah Celuch <noah.celuch@gmail.com>
2026-05-03 22:02:03 -0400

    feat(nvim): treesitter and tokyonight config

---
+++ configs/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,9 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.config").setup({ 
+      ensure_installed = { "lua", "python", "javascript", "typescript", "bash", "rust", "ruby" },
+    })
+  end,
+}

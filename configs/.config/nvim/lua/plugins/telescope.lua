d871eb4ff19fb8c0228cddc2a1e775b349296a3e
Noah Celuch <noah.celuch@gmail.com>
2026-05-03 22:21:35 -0400

    feat(nvim): nvim treesitter config

---
+++ configs/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,20 @@
+return {
+    'nvim-telescope/telescope.nvim',
+    version = '*',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-treesitter/nvim-treesitter',
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
+    config = function()
+      local telescope = require("telescope")
+
+      telescope.setup({
+        pickers = {
+          find_files = {
+            hidden = true,
+          }
+        }
+      })
+    end
+}
--- configs/.config/nvim/lua/plugins/treesitter.lua
+++ configs/.config/nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,8 @@ return {
   build = ":TSUpdate",
   config = function()
     require("nvim-treesitter.config").setup({ 
-      ensure_installed = { "lua", "python", "javascript", "typescript", "bash", "rust", "ruby" },
+      ensure_installed = { "lua", "python", "javascript", "typescript", "bash", "rust", "ruby", "go" },
+      highlight = { enable = true },
     })
   end,
 }
